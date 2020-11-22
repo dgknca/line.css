e2b633f0423963d685fa694756d205ab0939ee56
dgknca <dgknca@gmail.com>
2020-11-22 22:32:31 +0300

    blockquote colors

--- story/blockquotes/blockquote.template.js
+++ story/blockquotes/blockquote.template.js
@@ -1,11 +1,27 @@
+import { select } from '@storybook/addon-knobs'
+
 export default () => {
+  const colorClass = select(
+    'Color Class',
+    {
+      default: '',
+      'blockquote-orange': 'blockquote-orange',
+      'blockquote-green': 'blockquote-green',
+      'blockquote-red': 'blockquote-red',
+      'blockquote-pink': 'blockquote-pink',
+      'blockquote-blue': 'blockquote-blue',
+      'blockquote-yellow': 'blockquote-yellow'
+    },
+    ''
+  )
+
   return `
-  <blockquote class="blockquote" data-title="hey!">
+  <blockquote class="blockquote ${colorClass}" data-title="hey!">
     <span class="blockquote-text">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit purus mauris tellus eget. 
-      Justo elementum leo orci eget aliquet nunc. Arcu pharetra ultrices eu vitae, rutrum dictum purus et. 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit purus mauris tellus eget.
+      Justo elementum leo orci eget aliquet nunc. Arcu pharetra ultrices eu vitae, rutrum dictum purus et.
       Ac mauris in luctus nunc, sagittis.
     </span>
   </blockquote>
-  `;
-};
+  `
+}
--- story/inputs/radio.template.js
+++ story/inputs/radio.template.js
@@ -1,16 +1,19 @@
-import { select } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs'
 
 export default () => {
+  const colorClass = select(
+    'Color Class',
+    {
+      default: '',
+      'input-orange': 'input-orange',
+      'input-green': 'input-green',
+      'input-red': 'input-red',
+      'input-pink': 'input-pink',
+      'input-blue': 'input-blue',
+      'input-yellow': 'input-yellow'
+    },
+    ''
+  )
 
-  const inputClasses = select('Color Class', {
-    default: '',
-    'input-orange': 'input-orange',
-    'input-green': 'input-green',
-    'input-red': 'input-red',
-    'input-pink': 'input-pink',
-    'input-blue': 'input-blue',
-    'input-yellow': 'input-yellow',
-  }, '');
-
-  return `<label class="input-container input-check ${inputClasses}"><input type="radio" name="radioInput" /><span></span></label><label class="input-container input-check ${inputClasses}"><input type="radio" name="radioInput" /><span></span></label>`;
-};
+  return `<label class="input-container input-check ${colorClass}"><input type="radio" name="radioInput" /><span></span></label><label class="input-container input-check ${inputClasses}"><input type="radio" name="radioInput" /><span></span></label>`
+}
