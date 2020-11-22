44ed660726307c4f9188d76bd387aa5e63bc8d07
dgknca <dgknca@gmail.com>
2020-11-22 23:01:03 +0300

    image custom property

--- story/imgs/img.template.js
+++ story/imgs/img.template.js
@@ -1,9 +1,7 @@
-import { select, boolean } from '@storybook/addon-knobs'
+import { select, boolean, text } from '@storybook/addon-knobs'
 import classNames from 'classnames'
 
 export default () => {
-  const fullWidth = boolean('full width', false)
-
   const colorClass = select(
     'Color Class',
     {
@@ -29,12 +27,15 @@ export default () => {
     'img-sm'
   )
 
+  const fullWidth = boolean('full width', false)
+  const customPropertySize = text('--size', '')
+
   const isFullWidth = classNames({
     full: fullWidth
   })
 
   return `
-  <div class="img ${colorClass} ${sizeClass} ${isFullWidth}">
+  <div class="img ${colorClass} ${sizeClass} ${isFullWidth}" style="--size:${customPropertySize}">
     <img src="https://images.unsplash.com/photo-1605964883672-73ed3a8a29e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
   </div>
   `
